ea3f9503f1ba6891728f7b94e2c026bdbdbea594
Kavya Ganji <kavyaganji1901@gmail.com>
2019-12-25 22:09:47 +0530

    updated resume

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 3000
 app.use(express.static('public'))
 app.get('/', (req, res) => res.sendFile(__dirname+'/public/html/index.html'))
 app.get('/resume',(req , res) => res.sendFile(__dirname+'/public/html/resume.html'))
+app.get('/login',(req,res) => res.send('Login'))
+app.get('/register',(req,res) => res.send('Register'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 var obj={}
