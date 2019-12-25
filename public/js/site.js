3fa2590667046d41988e12f64b3645e0b72def6a
Kavya Ganji <kavyaganji1901@gmail.com>
2019-12-25 11:16:00 +0530

    Changes made

--- public/js/site.js
+++ public/js/site.js
@@ -1,10 +1,14 @@
+//page fully loaded
+// 1. get data from server - url ? /data
+// 2. show data in html
+
 $(document).ready(function()
 {
 $.getJSON("/data",function(d)
 {
 alert(JSON.stringify(d));
-$("name").text(d.name);
-$("col").text(d.college);
-$("reg").text(d.regno);
-}
+$("#name").text(d.name);
+$("#col").text(d.college);
+$("#reg").text(d.regno);
+})
 })
